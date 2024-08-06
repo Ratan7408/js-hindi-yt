3a887c8b21d05a1d01f692451dc7f16dca29a03e
RATAN SRIVASTAV <96727065+Ratan7408@users.noreply.github.com>
2024-08-06 20:10:17 +0000

    conversion done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber =  Number(score)
+console.log(typeof valueInNumber);
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1; false => 1
+
+let isLoggedIn =1
+let booleanIsLoggedIn = boolean(isLoggedIn);
+
+//1 => true ; 0=> false
+// "" => false
+
+
