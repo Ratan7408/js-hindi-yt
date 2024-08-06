5119165486a52a9ee2db2ae7ecd32480e42932d3
RATAN SRIVASTAV <96727065+Ratan7408@users.noreply.github.com>
2024-08-06 20:32:40 +0000

    done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,5 +17,69 @@ let booleanIsLoggedIn = boolean(isLoggedIn);
 
 //1 => true ; 0=> false
 // "" => false
+// ****************** Operations **************
+
+
+
+let value =3
+let negValue = -value
+console.log(negValue);
+// -3 output as i asigned
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // output => 2 to the power 2
+// console.log(2/3);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = "Ratan"
+
+let str3 = str1+str2 // output => hello Ratan
+
+
+console.log("1" +2);  // 12 
+console.log(1+"2");   //12
+console.log("1" +2 +2); // 122
+console.log(1 + 2 +"2"); // 32
+// ToPrimitive se hoti hai yeh sab and Type conversion
+
+
+console.log(true); // true
+console.log(true+); // errror
+console.log(+true); // +true
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);  //101
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
