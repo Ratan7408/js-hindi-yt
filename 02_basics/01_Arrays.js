a57c789875e2537d5280f4ae9ea491915e33a25f
RATAN SRIVASTAV <96727065+Ratan7408@users.noreply.github.com>
2024-08-09 17:36:30 +0000

    hh

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,47 @@
+// Arrays
+
+// const myArr = [0,2,2,3,4,5]
+// const myHeros = ["ratan","lala"]
+
+// console.log(myArr);  //[ 0, 2, 2, 3, 4, 5 ]
+// console.log(myArr[0]);  //0
+
+
+
+// // Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()  // last value ir 7 will be removed
+
+// myArr.unshift(9) // 9 will be added at first posotion
+// myArr.shift()  // one next element will be added
+
+// const newArr = myArr.join()
+// console.log(newArr);  // it will change in string fornmat
+
+// console.log(myArr);   //[
+//    // 0, 2, 2, 3,
+//    // 4, 5, 6
+//  // ]
+
+//  const myn1 = myArr.slice(1, 3)
+
+//  console.log(myn1);
+ 
+// // slice => 
+
+// // splice =>
+
+// const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+// console.log(animals.slice(2));
+// console.log(animals.slice(2, 4));
+
+
+
+
+
+
+
+
