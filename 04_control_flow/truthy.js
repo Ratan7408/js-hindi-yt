64e82848d3cc3007dffa4e0b4bba16e5382ee883
RATAN SRIVASTAV <96727065+Ratan7408@users.noreply.github.com>
2024-08-11 10:03:35 +0000

    truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,20 @@
+const userEmail = "ratan74082@gmail.com"
+
+if (userEmail) {
+    console.log("Got user email");
+  
+} else {
+    console.log("Don't have user email");
+}
+// for truthy falsy check notes
+
+// Nullisg Coalescing Operator (??): null defined
+
+let val1;
+//val1 = 5 ?? 10
+//val1 = null ?? 10
+//val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15
+
+
+console.log(val1);
