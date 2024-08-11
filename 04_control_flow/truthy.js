4cb5344070a6a39deaee76f5221c7b3a601d4f54
RATAN SRIVASTAV <96727065+Ratan7408@users.noreply.github.com>
2024-08-11 11:56:59 +0000

    iteration

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -11,10 +11,10 @@ if (userEmail) {
 // Nullisg Coalescing Operator (??): null defined
 
 let val1;
-//val1 = 5 ?? 10
-//val1 = null ?? 10
-//val1 = undefined ?? 15
-val1 = null ?? 10 ?? 15
+val1 = 5 ?? 10
+val1 = null ?? 10
+val1 = undefined ?? 15
+//val1 = null ?? 10 ?? 15
 
 
 console.log(val1);
