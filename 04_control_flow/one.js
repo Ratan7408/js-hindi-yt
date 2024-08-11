bd6a3d02c8560bde8db44b0f9c14ad80633eb769
RATAN SRIVASTAV <96727065+Ratan7408@users.noreply.github.com>
2024-08-11 09:39:51 +0000

    switch

---
+++ 04_control_flow/one.js
@@ -0,0 +1,29 @@
+// if
+const isUserLoggedIn = true
+const temperature = 60
+if( temperature < 50){
+    console.log("< that 50");
+    
+    
+}else{
+    console.log(console.log("temp is > 50"));
+    
+}
+
+//console.log("temp is > 50");
+
+const balance = 1000
+//if(balance > 500) console.log("test"),console.log("test2"); // dont do like this
+
+if(balance < 500){
+    console.log("less than");
+    
+}else if(balance < 750){
+    console.log("less than 750");
+    
+}else{
+    console.log("less than 1200");
+    
+}
+
+
